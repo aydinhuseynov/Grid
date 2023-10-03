d92fa98890f292a715898337372ef0455e4b49e2
aydinhuseynov <aydinnhuseynli@gmail.com>
2023-10-03 21:20:44 +0400

    this is grid examples

---
+++ first.js
@@ -0,0 +1,58 @@
+// document.querySelector("#number").style.color="red";
+
+// document.querySelector("#name").innerHTML="Real";
+
+// document.querySelector("#surname").textContent="Madrid";
+
+// function sum(a,b,c){
+//     return (a+b+c)/3
+// }
+// console.log(sum(3,4,11));
+
+// let sum =1;
+
+// for(let i =1; i<=3;i++){
+//     sum = sum * i;
+
+// }
+
+// console.log(sum);
+
+// for(let i=0; i<=arr.length-1; i++){ 
+//    sum = sum + arr[i];
+// }
+
+
+// let sum = 0;
+// for(let i=0; i<=arr.length-1;i++){
+// sum= (sum + arr[i])
+// edediOrta = sum/arr.length
+// }
+// console.log(edediOrta);
+
+// let arr = [3,4,11];
+// const result = arr.reduce((a,b)=>{
+//     return a+b;
+// })/arr.length
+
+// console.log(result);
+// let arr =  ["I","WILL",'BE','MASTER','IN','JAVASCRIPT']
+// let text = ""
+// for(let i = 0; i<arr.length;i++){
+//     text = text +" " + arr[i]
+// }
+// console.log(text);
+// let numbers = []
+// for(let i =1;i<10;i++){
+//  numbers = numbers + i 
+// }
+// console.log(numbers.toString());
+
+
+
+
+
+
+
+
+
